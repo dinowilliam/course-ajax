ffe147f4b381be09e299f1131f03fa5dcab1c53c
dinowilliam <dinowilliam.dev@gmail.comm>
2020-07-03 19:19:10 -0300

    Changes in the projec to make requisition to Unplash

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,12 +1,64 @@
 (function () {
     const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
+    const searchField = document.querySelector('#search-keyword');    
     const responseContainer = document.querySelector('#response-container');
+    let searchedForText = 'hippos';    
 
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+        resquestImages();
     });
+
+    resquestImages();
+    
+    function resquestImages(){
+        let unsplashRequest = new XMLHttpRequest();
+        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        unsplashRequest.setRequestHeader('Authorization', 'Client-ID ' + readTextFile("ClientID.txt") );
+        unsplashRequest.onload = addImage;
+        unsplashRequest.send()
+    };
+    
+    function addImage(){
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if(data && data.results && data.results[0]){
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+        }
+        else{
+            htmlContent = '<div class="error-no-image">No images available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    } 
+
+    function readTextFile(file){
+        var allText = '';
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function ()
+        {
+            if(rawFile.readyState === 4)
+            {
+                if(rawFile.status === 200 || rawFile.status == 0)
+                {
+                    allText = rawFile.responseText;
+                    
+                }
+            }
+        }
+        rawFile.send(null);
+
+        return allText;
+    }   
+
 })();
+
+
