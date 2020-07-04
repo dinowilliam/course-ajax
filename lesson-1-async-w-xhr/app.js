907a583e593d496b682ea0febabf8e1e7703949f
dinowilliam <dinowilliam.dev@gmail.comm>
2020-07-03 22:53:06 -0300

    Changes in GitIgnore and App.js

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -11,14 +11,16 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
         resquestImages();
+        resquestArticle();
     });
 
     resquestImages();
+    resquestArticle();
     
     function resquestImages(){
         let unsplashRequest = new XMLHttpRequest();
         unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-        unsplashRequest.setRequestHeader('Authorization', 'Client-ID ' + readTextFile("ClientID.txt") );
+        unsplashRequest.setRequestHeader('Authorization', `Client-ID ${readTextFile("UnsplashId.txt")}`);
         unsplashRequest.onload = addImage;
         unsplashRequest.send();
     };
@@ -31,7 +33,7 @@
             const firstImage = data.results[0];
             htmlContent = `<figure>
                 <img src="${firstImage.urls.regular}" alt="${searchedForText}">
-                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                <figcaption>${firstImage.description.replace(".","")} by ${firstImage.user.name}</figcaption>
             </figure>`;
         }
         else{
@@ -40,6 +42,31 @@
 
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     } 
+
+    function resquestArticle(){
+        let articleRequest = new XMLHttpRequest();        
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=${readTextFile("NYId.txt")}`);
+        articleRequest.onload = addArticles;
+        articleRequest.send();
+    }
+
+    function addArticles () {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+
+        if(data.response && data.response.docs && data.response.docs.length > 1){            
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+            </li>`).join('') + '</ul>'                
+        }
+        else{
+            htmlContent = '<div class="error-no-articles">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);        
+    }
+
 
     function readTextFile(file){
         var allText = '';        
